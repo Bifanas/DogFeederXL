bbb536ef1d728a6f82f09ee1d4651d9f9fdcd7b5
Natália <57140719+SoaresNatalia45@users.noreply.github.com>
2023-05-11 17:52:21 +0100

    Update som.ino
    funções adicionadas e comentários sobre o funcionamento

--- som.ino
+++ som.ino
@@ -1,54 +1,56 @@
 #include <SoftwareSerial.h>
-#include "RedMP3.h"
+#include "RedMP3.h" //MP3 maroto
 
-#define volume_Pot A0//SIG pin of Rotary Angle Sensor module connect to A0 of IO Shield, that is pin A0 of Arduino
-#define playpause 3
-#define ARDUINO_RX 7//RX of Serial MP3 module connect to D7 of Arduino
-#define ARDUINO_TX 8//TX to D8, note that D8 can not be used as RX on Mega2560, you should modify this if you donot use Arduino UNO
-MP3 mp3(ARDUINO_RX, ARDUINO_TX);
+#define volume_Pot A0
+#define ARDUINO_RX 7 //mp3
+#define ARDUINO_TX 8 //mp3
+MP3 mp3(ARDUINO_RX, ARDUINO_TX); //MP3
 
-int8_t musica1 = 0x01;
-int8_t musica2 = 0x02;
-int8_t musica3 = 0x03;
-int8_t folderName = 0x01;
-int8_t lastVolume;
-
+int8_t musica1 = 0x01; //primeira música do cartão SD
+int8_t musica2 = 0x02; //segunda música do cartão SD
+int8_t musica3 = 0x03; //terceira música do cartão SD
+int8_t folderName = 0x01; //pasta do cartão
+int8_t lastVolume; //volume atual
 
 void setup() {
-Serial.begin(9600);
-  delay(500);
-  mp3.setVolume(lastVolume);
- // mp3.playWithVolume(musica2, lastVolume);
+  Serial.begin(9600);
+  delay(500); //delay de 500ml pra iniciar o mp3
+  mp3.setVolume(lastVolume); //selecionar o volume
 }
 
-String str;
-void loop() { 
-   int volume = map(analogRead(volume_Pot),0,1024,0,30);
+void playTrack(String str, int8_t lastVolume){ //função das músicas
+  if(str == "1"){ //primeira música
+    mp3.playWithVolume(musica1, lastVolume);
+    Serial.println("First sound track.");
+  }
+  else if(str == "2"){ //segunda música
+    mp3.playWithVolume(musica2, lastVolume);
+    Serial.println("Second sound track.");
+  }
+  else if(str == "3"){ //terceira música
+    mp3.playWithVolume(musica3, lastVolume);
+    Serial.println("Third sound track.");
+  }
+}
+
+void adjustVolume(int8_t lastVolume){ //ajustar volume
+  int volume = map(analogRead(volume_Pot),0,1024,0,30); //mapeamento do volume de 1 a 30
   
   if (volume != lastVolume) {
     mp3.setVolume(volume);
     Serial.print("pot set to: ");Serial.println (volume_Pot);
-   Serial.print("volume set to: "); Serial.println (volume);
+    Serial.print("volume set to: "); Serial.println (volume);
     lastVolume = volume;
- }
- delay(50);
-if(Serial.available())
-{
-str = Serial.readStringUntil('\n');
-if(str == "1")
-{
-mp3.playWithVolume(musica1, lastVolume);//play the second track with volume 30 class
-Serial.println("First sound track.");
+  }
 }
-if(str == "2")
-{
-mp3.playWithVolume(musica2, lastVolume);//play the second track with volume 30 class
-Serial.println("Second sound track.");
-}
-if(str == "3")
-{
-mp3.playWithVolume(musica3, lastVolume);//play the third track with volume 30 class
-Serial.println("Third sound track.");
-}
-}
+
+void loop() { 
+  adjustVolume(lastVolume); //função do volume
+  
+  delay(50); //delay pra ajustar 
+  
+  if(Serial.available()){ //aqui ele le o serialbegin, mas na telinha seria o menu
+    String str = Serial.readStringUntil('\n');
+    playTrack(str, lastVolume);
+  }
 }

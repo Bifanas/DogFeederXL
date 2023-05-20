bdeb3c699e50201e0ffec9f056683aa3cf0065ac
Natália <57140719+SoaresNatalia45@users.noreply.github.com>
2023-05-20 10:15:33 +0100

    Update humidityandtemperature.ino
    update

--- humidityandtemperature.ino
+++ humidityandtemperature.ino
@@ -1,41 +1,31 @@
-#include <dht.h> //
+#include <dht.h>
 #define dataPin 8
-#define MAX_SIZE 10
 
 dht DHT;
 
-float temp[MAX_SIZE];
-float hum[MAX_SIZE];
-int index = 0;
-
-void setup(){
+void setup() {
   Serial.begin(9600);
-  for(int i = 0; i < MAX_SIZE; i++) {
-    temp[i] = 0.0;
-    hum[i] = 0.0;
-  }
 }
 
-void loop(){
-  readAndPrintDHTData();
-  delay(60000);
+void loop() {
+  float temperature, humidity;
+  readDHTData(temperature, humidity);
+  printDHTData(temperature, humidity);
+  delay(5000);
 }
 
-void readAndPrintDHTData() {
+void readDHTData(float& temperature, float& humidity) {
   int readData = DHT.read22(dataPin);
-  float t = DHT.temperature;
-  float h = DHT.humidity;
+  temperature = DHT.temperature;
+  humidity = DHT.humidity;
+}
 
-  temp[index] = t;
-  hum[index] = h;
-
+void printDHTData(float temperature, float humidity) {
   Serial.print("Temperature = ");
-  Serial.print(t);
+  Serial.print(temperature);
   Serial.print(" C | ");
   Serial.print("Humidity = ");
-  Serial.print(h);
+  Serial.print(humidity);
   Serial.println(" % ");
   Serial.println("");
-
-  index = (index + 1) % MAX_SIZE;  // this will keep index in the range of 0 to MAX_SIZE-1
 }
